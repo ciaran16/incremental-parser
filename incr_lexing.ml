eee5dfd3a47202082ad6e8804e677a69b56e2bbe
Ciaran Lawlor <ciaranlawlor16@gmail.com>
2017-02-07 20:29:57 +0000

    Add non_incremental lexing, start incremental

--- incr_lexing.ml
+++ incr_lexing.ml
@@ -3,9 +3,37 @@ type 'tok lex_result =
   | Error_msg of string
   | Error_with_token of 'tok * string
 
+type ('char, 'tok) lex_f = 'char Rope.Iterator.t -> 'tok lex_result * 'char Rope.Iterator.t
+
 let token tok = Token tok
 
 let error ?token msg =
   match token with
   | None -> Error_msg msg
   | Some token -> Error_with_token (token, msg)
+
+(* TODO need to do something with error messages. *)
+
+module Non_incremental = struct
+  let lex_all rope ~end_token lexer =
+    let rec loop iter =
+      match lexer iter with
+      | Error_msg _, iter -> loop iter
+      | Error_with_token (tok, _), iter -> tok :: loop iter
+      | Token tok, iter -> tok :: loop iter
+    in
+    rope |> Rope.iterator |> loop |> Rope.of_list
+end
+
+module Incremental = struct
+  type ('char, 'tok) t = {
+    rope : 'char Rope.t;
+    lexer : ('char, 'tok) lex_f;
+    end_token : 'tok;
+    tokens : 'tok Rope.t;
+  }
+
+  let make rope ~end_token lexer =
+    let tokens = lexer |> Non_incremental.lex_all rope ~end_token in
+    {rope; lexer; end_token; tokens}
+end
--- incr_lexing.mli
+++ incr_lexing.mli
@@ -1,5 +1,17 @@
 type 'tok lex_result
 
+type ('char, 'tok) lex_f = 'char Rope.Iterator.t -> 'tok lex_result * 'char Rope.Iterator.t
+
 val token : 'tok -> 'tok lex_result
 
 val error : ?token:'tok -> string -> 'tok lex_result
+
+module Non_incremental : sig
+  val lex_all : 'char Rope.t -> end_token:'tok -> ('char, 'tok) lex_f -> 'tok Rope.t
+end
+
+module Incremental : sig
+  type ('char, 'tok) t
+
+  val make : 'char Rope.t -> end_token:'tok -> ('char, 'tok) lex_f -> ('char, 'tok) t
+end
