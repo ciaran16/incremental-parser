4f75dfb72a09367f5b7940a8adb741fe99b60290
Ciaran Lawlor <ciaranlawlor16@gmail.com>
2017-02-21 22:03:31 +0000

    Rewrite incr_lexing

--- incr_lexing.ml
+++ incr_lexing.ml
@@ -1,13 +1,7 @@
-module Rope = Gadt_rope.String_rope
-module Iterator = Rope.Fast_iterator
-module F_array = Gadt_rope.Functional_array
-
 type 'tok lex_result =
   | Token of 'tok
   | Error_msg of string
   | Error_with_token of 'tok * string
-
-type 'tok lex_f = char Iterator.t -> 'tok lex_result * char Iterator.t
 
 let token tok = Token tok
 
@@ -16,32 +10,28 @@ let error ?token msg =
   | None -> Error_msg msg
   | Some token -> Error_with_token (token, msg)
 
-(* TODO need to do something with error messages. *)
+type 'tok lex_f = int -> 'tok lex_result * int
 
-module Non_incremental = struct
-  (* TODO this needs doing properly. *)
-  let lex_all rope ~end_token lexer =
-    let rec loop iter =
-      if Iterator.is_at_end iter then [end_token]
-      else
-        match lexer iter with
-        | Error_msg _, iter -> loop iter
-        | Error_with_token (tok, _), iter | Token tok, iter -> tok :: loop iter
-    in
-    match rope |> Iterator.start_at 0 with
-    | None -> F_array.of_array [||]
-    | Some iter -> loop iter |> Array.of_list |> F_array.of_array
-end
-
-module Incremental = struct
+module Lexer = struct
   type 'tok t = {
-    rope : char Rope.t;
-    lexer : 'tok lex_f;
-    tokens : 'tok F_array.t;
-    end_token : 'tok;
+    lex : 'tok lex_f;
+    pos : int;
+    next : ('tok * int) Lazy.t;
   }
 
-  let make rope ~end_token lexer =
-    let tokens = lexer |> Non_incremental.lex_all rope ~end_token in
-    {rope; lexer; tokens; end_token}
+  let start_at pos lex =
+    (* TODO need to do something with error messages. *)
+    let rec lex_until_token pos = match lex pos with
+      | Token token, len | Error_with_token (token, _), len -> token, pos + len
+      | Error_msg _, len -> lex_until_token (pos + len)
+    in
+    {lex; pos; next = lazy (lex_until_token pos)}
+
+  let next {lex; next; _} = let token, pos = Lazy.force next in token, lex |> start_at pos
+
+  let peek {next; _} = Lazy.force next |> fst
+
+  let skip n {lex; pos; _} = lex |> start_at (pos + n)
+
+  let pos {pos; _} = pos
 end
--- incr_lexing.mli
+++ incr_lexing.mli
@@ -1,21 +1,21 @@
-module Rope = Gadt_rope.String_rope
-module Iterator = Rope.Fast_iterator
-module F_array = Gadt_rope.Functional_array
-
 type 'tok lex_result
-
-type 'tok lex_f = char Iterator.t -> 'tok lex_result * char Iterator.t
 
 val token : 'tok -> 'tok lex_result
 
 val error : ?token:'tok -> string -> 'tok lex_result
 
-module Non_incremental : sig
-  val lex_all : char Rope.t -> end_token:'tok -> 'tok lex_f -> 'tok F_array.t
-end
+type 'tok lex_f = int -> 'tok lex_result * int
 
-module Incremental : sig
+module Lexer : sig
   type 'tok t
 
-  val make : char Rope.t -> end_token:'tok -> 'tok lex_f -> 'tok t
+  val start_at : int -> 'tok lex_f -> 'tok t
+
+  val next : 'tok t -> 'tok * 'tok t
+
+  val peek : 'tok t -> 'tok
+
+  val skip : int -> 'tok t -> 'tok t
+
+  val pos : 'tok t -> int
 end
